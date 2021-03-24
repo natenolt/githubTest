9a13e6f1deddc7c274e29cbe71f97267cb2d2a94
nate nolting <knate89.333@gmail.com>
2021-03-23 20:38:36 -0400

    importing files

---
+++ minMax/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace minMax
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
